578e1ab3bcf15630ac2e49dbb5489730134e3626
Donatas Vitkus <vitk.donatas@gmail.com>
2022-01-27 22:10:16 +0200

    first commit

---
+++ ND Prekes Apsipirkimas Israsas Cekis Email/ND Prekes Apsipirkimas Israsas Cekis Email/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ND_Prekes_Apsipirkimas_Israsas_Cekis_Email
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
